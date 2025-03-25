45cd5170d292b4ab447b882b9e5e437879ff99f7
coderismail22 <coderismail22@gmail.com>
2025-03-25 11:54:14 +0600

    update search bar

--- src/Pages/SearchResults/SearchBar.tsx
+++ src/Pages/SearchResults/SearchBar.tsx
@@ -19,7 +19,7 @@ const SearchBar = () => {
         placeholder="Search articles..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="border px-3 py-2 rounded-md w-64"
+        className="border px-3 py-2 rounded-md w-64 bg-white"
       />
       <button
         type="submit"
